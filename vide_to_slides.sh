e516c6321e49549e8a92c0d7e9b6c8e4505f2655
BodeSpezial <konstantin.bode@outlook.com>
2021-05-05 17:13:40 +0200

    Add script to download and convert videos from youtube to presentations (html or pptx)

---
+++ vide_to_slides.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# fetch rss feed
+curl -s "https://www.youtube.com/feeds/videos.xml?channel_id=UCOY3VWW4NGQAue_rqTBtdhA" >> newest_videos.tmp
+
+# Extract the video_Url from the rss feed
+video_Url=$(cat newest_videos.tmp | grep -iF media:content | head -n1 | awk -F'"' '{ print $2 }')
+
+# download the video
+youtube-dl "$video_Url" -o 'newest_video.mp4' -f mp4
+
+# download the subtitles
+youtube-dl --write-auto-sub --sub-lang de --skip-download "$video_Url"
+
+# split the video in frames
+mkdir frames
+ffmpeg -i newest_video.mp4 -r 2 frames/%05d.bmp
+
+# create the presentation
+ls frames/ | awk '{ print "![](./frames/"$0")\n" }' >> pres.tmp
+
+# create the powerpoint presentation
+# pandoc -s pres.tmp -o video_slides.pptx
+# create the reveal.js presentation
+pandoc -s pres.tmp -t revealjs -o video_slides.html
+
+# clean the temporary files
+rm -rf *.tmp newest_video.mp4 video_slides.txt frames
+
